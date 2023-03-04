053091f01e1b2017277fb2dddaa64ba68123d4e3
Viktor <binjospookie@gmail.com>
2023-03-04 12:47:06 +0300

    added: correct types for createForm

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,14 @@
-import { allSettled, clearNode, createApi, createDomain, createEffect, createEvent, fork, launch } from 'effector';
+import { createDomain } from 'effector';
 import { createForm as ffCreateForm } from 'final-form';
 
-const createFormApi = (form: ReturnType<typeof ffCreateForm>) => {
-  const change = createEffect<{ name: string; value: string }, void>((x) => form.change(x.name, x.value));
+import type { Config as FFConfig } from 'final-form';
 
-  return {
-    change,
-  };
-};
+type FormConfig<T> = Omit<FFConfig<T>, 'mutators' | 'debug' | 'destroyOnUnregister' | 'keepDirtyOnReinitialize'>;
 
-const createForm = () => {
+const createForm = <FormValues>(config: FormConfig<FormValues>) => {
   const domain = createDomain();
 
-  const form = ffCreateForm({ onSubmit: console.log });
+  const form = ffCreateForm(config);
 
-  form.registerField('username', () => {}, { value: true });
-
-  const $values = domain.store({});
-
-  // https://effector.dev/docs/api/effector/scopebind/
-  form.subscribe(
-    (x) => {
-      launch($values, x.values);
-    },
-    { values: true },
-  );
-
-  const api = createFormApi(form);
-
-  return { $values, api, domain };
+  return { form };
 };
-
-const { $values, api, domain } = createForm();
-const { $values: $2, api: api2 } = createForm();
-
-$values.watch((x) => console.log(`$1 ${JSON.stringify(x, undefined)}`));
-$2.watch((x) => console.log(`$2 ${JSON.stringify(x, undefined)}`));
-api.change({ name: 'username', value: 'biba' });
-api.change({ name: 'username', value: 'boba' });
-
-api2.change({ name: 'lol', value: 'kek' });
-
-clearNode(domain, { deep: true });
-
-console.log('clear domain');
-
-api2.change({ name: 'should', value: 'work' });
-
-const scope = fork();
-
-await allSettled(api2.change, { scope, params: { name: 'scope', value: 'works' } });
-
-console.log(scope.getState($2), '$2 with scope');
