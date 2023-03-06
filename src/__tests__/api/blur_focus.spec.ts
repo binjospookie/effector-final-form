961ac2627daaf4b744b31de0a02581b876a54036
Viktor <binjospookie@gmail.com>
2023-03-06 12:23:12 +0300

    added: blur_focus test

---
+++ src/__tests__/api/blur_focus.spec.ts
@@ -0,0 +1,34 @@
+import { allSettled, fork } from 'effector';
+
+import { createForm } from '../../index';
+
+const onSubmitMock = () => {};
+
+describe('active', () => {
+  const { $formState, $fields, domain, api } = createForm({
+    onSubmit: onSubmitMock,
+    initialValues: { firstName: '' },
+    subscribeOn: ['active'],
+  });
+  const scope = fork(domain);
+
+  test('api.focusFx', async () => {
+    await allSettled(api.registerField, { scope, params: { name: 'firstName' } });
+
+    expect(scope.getState($formState).active).toBe(null);
+    expect(scope.getState($fields).firstName.active).toBe(false);
+
+    await allSettled(api.focusFx, { scope, params: 'firstName' });
+    expect(scope.getState($formState).active).toBe('firstName');
+    expect(scope.getState($fields).firstName.active).toBe(true);
+  });
+
+  test('api.blurFx', async () => {
+    expect(scope.getState($formState).active).toBe('firstName');
+    expect(scope.getState($fields).firstName.active).toBe(true);
+
+    await allSettled(api.blurFx, { scope, params: 'firstName' });
+    expect(scope.getState($formState).active).toBe(null);
+    expect(scope.getState($fields).firstName?.active).toBe(false);
+  });
+});
