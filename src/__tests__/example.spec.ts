18b33163e5bffbfca9053ab41b08a7051d7087a6
Viktor <binjospookie@gmail.com>
2023-03-07 11:14:38 +0300

    added: base example

--- src/__tests__/example.spec.ts
+++ src/__tests__/example.spec.ts
@@ -1,37 +1,7 @@
-# effector-final-form
-
-☄️ Effector bindings for Final Form 🏁 
-
-[Demo](https://stackblitz.com/edit/react-ts-xjh6yd?file=index.tsx)
-
-Forms values and validation rules are part of the business logic. This means that you need to be able to work with them without being bound to the UI-framework.
-
-The goal of this project is to combine [Final Form](https://final-form.org/) and [Effector](https://effector.dev/) to achieve the goal described above.
-
-## Installation
-
-```bash
-yarn add effector-final-form
-# or
-npm add effector-final-form
-# or
-pnpm add effector-final-form
-```
-
-## Usage
-
-Just import from the root module:
-
-```ts
-import { createForm } from 'effector-final-form';
-```
-
-## Base example
-
-```ts
 import { allSettled, fork } from 'effector';
-import { createForm } from 'effector-final-form';
 import { vi } from 'vitest';
+
+import { createForm } from '../index';
 
 vi.useFakeTimers();
 
@@ -113,4 +83,4 @@ describe('example', () => {
       expect($formState.getState().submitErrors).toBe(null);
     }
   });
-```
+});
